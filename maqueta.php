830b586a27f1b0b97dd58c124aa786db23c86bf4
esnalca <esnalca88@gmail.com>
2014-06-26 04:09:22 +0200

    Maqueta Añadir anuncios:añadido formulario

--- maqueta.php
+++ maqueta.php
@@ -3,15 +3,15 @@ include("includes/header.php")
 ?>
         <div class="container">
             <section>
-                <div class="row">
+                <div class="row " >
                     <div class="col-lg-12">
                         <div class="row borde">
                             <div class="hidden-xs col-sm-3 col-md-3 col-lg-3 borde ">
                                 <h3 class="borde">Anuncios sección</h3>
                                 <ul class="borde">
-                                    <li class="borde">Añadir</li>
-                                    <li class="borde">Modificar</li>
-                                    <li class="borde">Eliminar</li>
+                                    <li class="color">Añadir</li>
+                                    <li class="color">Modificar</li>
+                                    <li class="color">Eliminar</li>
                                 </ul>
                             </div>
                             <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 borde">
@@ -36,7 +36,7 @@ include("includes/header.php")
                                             <div class="text">*</div>
                                             </div>
                                         </div>
-                                        <button class="btn btn-primary">Send</button>
+                                        <button class="btn btn-primary fondom">Send</button>
                                     </form>
                                 </div>
                             </div>
