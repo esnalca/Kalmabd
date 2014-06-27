81af81541e7d5a050d10115e982bfb20c1b71677
esnalca <esnalca88@gmail.com>
2014-06-27 03:33:21 +0200

    Sube archivos añade anuncios con imagenes añadido include forma y parametros para acceder añadir

--- guardaranun.php
+++ guardaranun.php
@@ -1,24 +1,27 @@
 <?
-include("includes/conexion");
+
+include("includes/conexion.php");
+include("includes/header.php");
+
 			$tit=$_POST["tit"];
             $des=$_POST["des"];
             $img=$_FILES["archivo"]["name"];
             
 
 	if($tit!="" and $des!="" and $img!="" ){
-		$ruta="/img/varias/";
-			if($_FILES["archivo"]["error"]==0){
-				echo $tit ."<br>". $des ."<br>". $img."<br>". $_FILES["archivo"]["error"] ."<br>". $ruta."<br>".$_FILES["archivo"]["tmp_name"];
-			//move_uploaded_file($_FILES["archivo"]["tmp_name"],$ruta);
-			//	$sql="INSERT INTO  `kalmap`.`anupor` (`id` ,`tit` ,`des` ,`img`)
-			//		  VALUES (NULL ,  '$tit',  '$des',  '$img')";
-			//	$res=mysqli_query($link,$sql);				
-				
+		$ruta= "img/varias/";
+			if($_FILES["archivo"]["error"]==0){;
+				if(move_uploaded_file($_FILES["archivo"]["tmp_name"],$ruta.$img)){
+				$sql="INSERT INTO  `kalmap`.`anupor` (`tit` ,`des` ,`img`)
+					  VALUES ('$tit',  '$des',  '$img')";
+				$res=mysqli_query($link,$sql);	
+				header("Location:maqueta.php?a=2");
+			}		
 			}
 	}else{
 		echo "variables vacias";
 	}
-
-
+			
+include("includes/footer.php");
 ?>
 
--- maqueta.php
+++ maqueta.php
@@ -6,40 +6,21 @@ include("includes/header.php")
                 <div class="row " >
                     <div class="col-lg-12">
                         <div class="row borde">
-                            <div class="hidden-xs col-sm-3 col-md-3 col-lg-3 borde ">
+                            <div class="hidden-xs col-sm-3 col-md-3 col-lg-3  ">
                                 <h3>Anuncios sección</h3><hr/>
-                                <ul class="borde">
-                                    <li class="color">Añadir</li>
+                                <ul class="">
+                                    <a href="maqueta.php?a=1"><li class="color">Añadir</li></a>
                                     <li class="color">Modificar</li>
                                     <li class="color">Eliminar</li>
                                 </ul>
                             </div>
-                            <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-                                <h3>Añadiendo anuncios seccion</h3><hr/>
-                                <div class="container borde">
-                                    <form role ="form" enctype="multipart/form-data" style="width:400px;" action="guardaranun.php"  method="POST" >
-                                        <div class="required-field-block">
-                                            <input type="text" placeholder="Titulo" name="tit" class="form-control">
-                                            <div class="required-icon">
-                                            <div class="text">*</div>
-                                            </div>
-                                        </div>
-                                        <div class="required-field-block">
-                                            <input type="file" placeholder="Imagen" name="archivo" class="form-control">
-                                            <div class="required-icon">
-                                            <div class="text">*</div>
-                                            </div>
-                                        </div>
-                                        <div class="required-field-block">
-                                            <textarea rows="3" class="form-control" name="des" placeholder="Descripcion"></textarea>
-                                            <div class="required-icon">
-                                            <div class="text">*</div>
-                                            </div>
-                                        </div>
-                                        <button class="btn btn-primary fondom">Guardar</button>
-                                    </form>
-                                </div>
-                            </div>
+                            <?
+                            if($_GET["a"]==1){
+                                include("includes/datos/forma.html");
+                                }if($_GET["a"]==2){
+                                    echo "<h2>Anuncio añadido correctamente</h2>";
+                                }
+                            ?>
                         </div>
                     </div>
                 </div>
